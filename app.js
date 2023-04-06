ed11e804f75b5c638e26d21782066a46454f527b
Mohd Rejoan <wearecrx@gmail.com>
2023-04-06 12:54:02 +0600

    auth-header added

--- app.js
+++ app.js
@@ -4,8 +4,26 @@ const AdminRoute = require("./src/routes/AdminRoute");
 const bodyParser = require("body-parser");
 
 // package setup
-
 const app = express();
+
+// set header
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:1212");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-Type, Authorization"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 
 // parse data
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +38,7 @@ app.use(AdminRoute);
 
 // student routes
 
-Database.sync({force: true}).then(() => {
+Database.sync().then(() => {
     app.listen(5000, () => {
         console.log(`Server listening on port http://localhost:${5000}`);
     });
