f6f7c971ace3a9a8ca7935732086b83b11d6b475
Mohd Rejoan <wearecrx@gmail.com>
2023-05-14 14:06:11 +0600

    notice file url section added on database

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use(CommonRoute);
 app.use(NoticeRoute);
 app.use(FilesRoutes);
 app.use((req, res, next) => {
-    res.status(404).json({ message: 'Not found' });
-})
+    res.status(404).json({ message: "Not found" });
+});
 
 // trainer routes
 
--- src/routes/NoticeRoutes.js
+++ src/routes/NoticeRoutes.js
@@ -1,35 +1,57 @@
 const express = require("express");
 const router = express.Router();
 const NoticeController = require("../controller/NoticeController");
-const multer = require('multer');
+const multer = require("multer");
+const fs = require("fs/promises");
 
-
-
+const noticeFilePath = "./uploads/notice";
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, '../../uploads');
-  },
-  filename: function (req, file, cb) {
-    console.log('hello rejoan')
-    console.log(req)
-    cb(null, file.filename);
-  }
-})
+    destination: async function (req, file, cb) {
+        // Check if the directory exists
+        fs.access(noticeFilePath, fs.constants.F_OK)
+            .then((data) => {
+                cb(null, "uploads/");
+            })
+            .catch((err) => {
+                if (err) {
+                    // Directory does not exist, create it
+                    fs.mkdir(noticeFilePath)
+                        .then(() => {
+                            cb(null, "uploads/");
+                        })
+                        .catch((err) => {
+                            if (err) {
+                                return;
+                            }
+                        });
+                } else {
+                    cb(null, "uploads/");
+                }
+            });
+    },
+    filename: function (req, file, cb) {
+        const uniqueId = Date.now();
+        const extension = file?.originalname?.substring(
+            file?.originalname?.lastIndexOf(".")
+        );
+        const fileName = `notice_${uniqueId}${extension}`;
+        cb(null,fileName);
+    },
+});
 
 const uploadConfig = multer({ storage });
 
 // notice router
-router.post("/notice/create", uploadConfig.single("file"), NoticeController.createNotice);
+router.post(
+    "/notice/create",
+    uploadConfig.single("file"),
+    NoticeController.createNotice
+);
 router.put("/notice/update", NoticeController.updateNotice);
 router.delete("/notice/delete", NoticeController.deleteNotice);
 router.get("/notice/all", NoticeController.all);
-router.get('/notice/classroom', NoticeController.classRoom);
+router.get("/notice/classroom", NoticeController.classRoom);
 router.get("/notice/training-center", NoticeController.trainingCenter);
-
-
-
-
-
 
 module.exports = router;
